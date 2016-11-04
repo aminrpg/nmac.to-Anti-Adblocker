c573bcf9f03d84965c619bdcbf54992e1d916990
Amin Sepehri <aminrpg@gmail.com>
2016-11-04 15:08:21 +0330

    Update nmac.to-Anti-Adblocker.user.js

--- nmac.to-Anti-Adblocker.user.js
+++ nmac.to-Anti-Adblocker.user.js
@@ -3,15 +3,16 @@
 // @namespace    http://netramz.com/
 // @version      0.1
 // @description  Get rid of Anti-Adblocker on nmac.to website
-// @author       aminrpg
+// @author       AMIN_RPG
 // @match        http://nmac.to/*
 // @grant        none
 // ==/UserScript==
-// ==OpenUserJS==
-// @author aminrpg
-// ==/OpenUserJS==
 
 (function() {
     'use strict';
     var _0xd71a='';
+    function fMUTjmKYha(){return false;}
+    jQuery('script[type*=javascript]:last').remove();
 })();
+
+
